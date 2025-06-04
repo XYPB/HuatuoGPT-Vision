68005180743c88fc99a29756f382a9954c5b83c9
YuexiDu <v-yuexidu@microsoft.com>
2025-06-04 07:02:15 +0000

    eval

---
+++ eval_huatuo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python eval_huatuo.py --num_samples -1
+python eval_huatuo.py --num_samples -1 --dataset PMC-VQA
+python eval_huatuo.py --num_samples -1 --dataset VQA-RAD
+python eval_huatuo.py --num_samples -1 --dataset OmniMedVQA
+python eval_huatuo.py --num_samples -1 --dataset PVQA
+python eval_huatuo.py --num_samples -1 --dataset SLAKE
