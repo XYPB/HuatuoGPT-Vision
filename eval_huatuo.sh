39c033824f748399a821719918756aa9198da27c
YuexiDu <v-yuexidu@microsoft.com>
2025-07-16 09:45:01 +0000

    fix

--- eval_huatuo.sh
+++ eval_huatuo.sh
@@ -4,15 +4,15 @@ set -e
 set -x
 source activate base
 conda activate vllm
+export CUDA_VISIBLE_DEVICES=2
 # python eval_huatuo.py --num_samples -1
 # python eval_huatuo.py --num_samples -1 --dataset PMC-VQA
-# python eval_huatuo.py --num_samples -1 --dataset VQA-RAD
+python eval_huatuo.py --num_samples -1 --dataset VQA-RAD
 # python eval_huatuo.py --num_samples -1 --dataset OmniMedVQA
 # python eval_huatuo.py --num_samples -1 --dataset PVQA
 # python eval_huatuo.py --num_samples -1 --dataset SLAKE
 # export CUDA_VISIBLE_DEVICES=2,3; python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region
 
-export CUDA_VISIBLE_DEVICES=0
 # python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region --bbox_coord
 # python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region --side_by_side
 # python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region --skip_region
@@ -25,4 +25,4 @@ export CUDA_VISIBLE_DEVICES=0
 # python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region_yn_hard --skip_region
 # python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region_yn_hard --side_by_side
 
-python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region_yn_clean_no_region
+# python eval_huatuo.py --num_samples -1 --dataset MeCoVQA_region_yn_clean_no_region
